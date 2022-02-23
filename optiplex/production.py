5caf035cb1c9009e9188d72ade8e6008d712238e
Oliver <oli1111@web.de>
2022-02-23 05:39:24 +0100

    impl. session for Production, and more
    - persistent(session) stock
    - persistent(session) production
    - stock vs prodution calculation
    - adding to stock
    - reseting stock
    
    using server-side session has simplified much of the program

--- optiplex/production.py
+++ optiplex/production.py
@@ -1,5 +1,4 @@
-import functools
-import sqlite3
+import copy
 import csv
 
 from flask import (
@@ -11,37 +10,21 @@ import optiplex.production_model as model
 
 bp = Blueprint('production', __name__, url_prefix='/production')
 
-def todict(obj, classkey=None):
-    if isinstance(obj, dict):
-        data = {}
-        for (k, v) in obj.items():
-            data[k] = todict(v, classkey)
-        return data
-    elif hasattr(obj, "_ast"):
-        return todict(obj._ast())
-    elif hasattr(obj, "__iter__") and not isinstance(obj, str):
-        return [todict(v, classkey) for v in obj]
-    elif hasattr(obj, "__dict__"):
-        data = dict([(key, todict(value, classkey))
-            for key, value in obj.__dict__.items()
-            if not callable(value) and not key.startswith('_')])
-        if classkey is not None and hasattr(obj, "__class__"):
-            data[classkey] = obj.__class__.__name__
-        return data
-    else:
-        return obj
 
-
-@bp.route('/', methods=['GET', 'POST'])
-def main():
-    db = get_db()
-
-    production_json = request.json
+@bp.route('/', methods=['GET'])
+def prod():
     content = {}
-    if production_json is not None:
-        production = model.from_dict(production_json)
-        content['materials'] = decorate_type(production.collect_materials({}), db)
-        content['blueprints'] = decorate_type(production.collect_blueprints({}), db)
+
+    if 'production' in session:
+        production = copy.deepcopy(session['production'])    # dont chage the main object
+        if 'stock' in session:
+            stock = copy.deepcopy(session['stock'])
+            production.update_vs_stock(stock)
+
+        content['materials'] = list(production.collect_materials({}).values())
+        content['materials'].sort(key=lambda x: x['depth'])
+        content['blueprints'] = list(production.collect_blueprints({}).values())
+        content['blueprints'].sort(key=lambda x: x['depth'])
 
     return render_template('production.html', content=content)
 
@@ -64,55 +47,57 @@ def find():
 @bp.route('/blueprint', methods=['GET'])
 def blueprint():
     bp_id = int(request.args.get("id"))
+    me = (100 - int(request.args.get("me"))) / 100
+    runs = int(request.args.get("runs"))
+
     db = get_db()
-    bpo = model.from_db(db, bp_id)
-    bpo = model.from_dict(vars(bpo))
-    return todict(bpo)
+    bpo = model.from_db(db, bp_id, me, runs)
+    session['production'] = bpo
+    return redirect(url_for('.prod'))
 
 
-@bp.route('/lookup', methods=['POST'])
+@bp.route('/lookup', methods=['GET'])
 def lookup():
     mat_id = request.args.getlist("id")
+    me = (100 - int(request.args.get("me"))) / 100
     db = get_db()
-    bpo = model.from_dict(request.json)
+    bpo = session['production']
 
-    if isinstance(mat_id, list):
-        for mid in mat_id:
-            bpo.produce_material(db, int(mid))
+    for mid in mat_id:
+        bpo.produce_material(db, int(mid), me=me)
+
+    session['production'] = bpo
+    return redirect(url_for('.prod'))
 
 
-    return todict(bpo)
-
-
-@bp.route('/clipboard', methods=['POST'])
+@bp.route('/clipboard', methods=['POST', 'DELETE'])
 def clipboard():
     if request.method == 'POST':
         text = request.json['data']
-
         db = get_db()
 
         clip = list(csv.reader(text.replace(',', '').replace('*', '').split('\n'), delimiter='\t'))
-        stock = {}
+        if 'stock' in session:
+            stock = session['stock']
+        else:
+            stock = {}
+
         for entry in clip:
             name = entry[0]
             count = int(entry[1])
 
-            row = db.execute("SELECT id, name FROM typeIDs WHERE name LIKE ?", [name]).fetchone()
-            stock[row[0]] = {'id': row[0], 'name': row[1], 'count': count}
+            row = db.execute("SELECT id, name, group_id FROM typeIDs WHERE name LIKE ?", [name]).fetchone()
+            if row[0] in stock:
+                stock[row[0]]['count'] += count
+            else:
+                stock[row[0]] = {'id': row[0], 'name': row[1], 'count': count, 'gid': row[2]}
 
         print(stock)
+        session['stock'] = stock
         return stock
 
+    if request.method == 'DELETE' and 'stock' in session:
+        session.pop('stock')
+
     return {}
-
-
-def decorate_type(types, db: sqlite3.Connection):
-    t_list = []
-    for k in types:
-        name = db.execute("SELECT name FROM typeIDs WHERE id=?;", [k]).fetchone()[0]
-        icon = f"Type/{k}_64.png"
-        t_list.append({'id': k, 'name': name, 'num': types[k], 'icon': icon})
-
-    return t_list
-
 
