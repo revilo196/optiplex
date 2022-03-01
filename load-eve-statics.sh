d07e2f9aa4afc4eccfde63b0b85211aef9c13534
Oliver <oli1111@web.de>
2022-03-01 02:46:34 +0100

    reaction uses ccp image server

--- load-eve-statics.sh
+++ load-eve-statics.sh
@@ -10,8 +10,9 @@ mkdir optiplex/import
 curl https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip > /tmp/sde.zip
 unzip /tmp/sde.zip -d optiplex/import/
 
+# outdated items are used from upto date CCP-server instead
 # load icons
-curl https://content.eveonline.com/data/Invasion_1.0_Icons.zip > /tmp/items.zip
-unzip /tmp/items.zip -d optiplex/static/
+# curl https://content.eveonline.com/data/Invasion_1.0_Icons.zip > /tmp/items.zip
+# unzip /tmp/items.zip -d optiplex/static/
 
 
